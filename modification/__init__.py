b6890174c9963276b446143e7e26498399bdced7
Amadeus Weber <amadeus@weber-live.de>
2024-08-13 08:30:58 +0000

    srt-file modification

---
+++ modification/__init__.py
@@ -0,0 +1,5 @@
+from modification.srt import modify_srt_files
+
+__all__ = (
+    'modify_srt_files',
+)
---
+++ modification/srt.py
@@ -0,0 +1,92 @@
+# Imports
+import os
+import re
+import datetime
+from dataclasses import dataclass
+from utils.io import get_files_by_extension, read_text_file, write_text_file
+from utils.datetime import (
+    time_total_seconds,
+    time_from_seconds,
+    strftime_hmsmilli
+)
+
+# Logging
+import logging
+logger = logging.getLogger(__name__)
+
+# Constants
+EXTENSION = '.srt'
+## Regex
+REGEX_EXTRACT = re.compile(
+    r"(\d+)\n(\d+\:\d+\:\d+,\d+) --> (\d+\:\d+\:\d+,\d+)\n((?:[^\n]+\n?)+)\n*"
+)
+
+# Functions
+def load_srt_file(path):
+    srt_text = ''.join(read_text_file(path))
+    for index, start, end, text in REGEX_EXTRACT.findall(srt_text):
+        yield Subtitle(
+            index,
+            datetime.time.fromisoformat(start),
+            datetime.time.fromisoformat(end),
+            text
+        )
+
+def write_srt_file(subtitles:list, path):
+    logger.info(f"Writing {path}")
+    text = '\n'.join(map(str, subtitles))
+    write_text_file(path, text)
+
+
+def modify_srt_file(path, fps_source, fps_output):
+    logger.info(f"Processing '{path}'")
+    subtitles = tuple(load_srt_file(path))
+    logger.info(subtitles[0].start)
+    logger.info(time_total_seconds(subtitles[0].start))
+    logger.info(time_from_seconds(time_total_seconds(subtitles[0].start)))
+
+    # Multipy start and end times by conversion factor
+    factor = fps_source / fps_output
+    for subtitle in subtitles:
+        subtitle.start = time_from_seconds(
+            time_total_seconds(subtitle.start) * factor
+        )
+        subtitle.end = time_from_seconds(
+            time_total_seconds(subtitle.end) * factor
+        )
+
+    # Save subtitles
+    write_srt_file(
+        subtitles,
+        os.path.splitext(path)[0] + '.fps' + str(fps_output) + '.srt'
+    )
+
+
+    
+
+def modify_srt_files(source, fps_source, fps_output):
+    # scan for files
+    srt_files = tuple(get_files_by_extension(source, EXTENSION))
+    logger.info('Found files: \n - ' + '\n - '.join(srt_files))
+
+    # process files
+    for file in srt_files:
+        modify_srt_file(file, fps_source, fps_output)
+
+# Classes
+@dataclass
+class Subtitle:
+    index:int
+    start:datetime.time
+    end:datetime.time
+    text:str
+
+    def __str__(self) -> str:
+        return '\n'.join((
+            str(self.index),
+            strftime_hmsmilli(self.start) + ' --> ' + 
+            strftime_hmsmilli(self.end),
+            self.text
+        ))
+    
+    
